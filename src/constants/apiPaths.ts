deada461edf8d34308536bb0b4d5be9d12e35334
rumyantsev <LemToUp@users.noreply.github.com>
2021-10-21 10:44:52 +0300

    Update product url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,6 +1,6 @@
 
 const API_PATHS = {
-  product: 'https://hbfj2voffb.execute-api.eu-west-1.amazonaws.com/dev',
+  product: 'https://fkl4atpmn5.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://qhqt1r9lzh.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
